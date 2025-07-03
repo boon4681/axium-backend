7c94f822591decf1201fdf23aa6679b9b8d42075
applentk <tanoobguy12@gmail.com>
2025-07-04 03:08:21 +0700

    update: setup and run API

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-import axium
+from axium.core import Axium
+from api.routes import node, execute, project
 
-some_func = axium.executor["sum_test"]
-print(some_func(1, 2))
+from fastapi import FastAPI
+
+Axium.setup()
+
+api = FastAPI(root_path="/v1/api")
+api.include_router(node.router)
+api.include_router(execute.router)
+api.include_router(project.router)
