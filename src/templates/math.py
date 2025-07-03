5117abbfe45d6a2e222f075c4cbc13259aed6b9c
applentk <tanoobguy12@gmail.com>
2025-07-04 03:12:13 +0700

    add: Axium's templates

---
+++ src/templates/math.py
@@ -0,0 +1,23 @@
+from axium.template import AxiumTemplate
+
+class PositivePlusNegative(AxiumTemplate):
+
+    input = {
+        "a": int,
+        "b": int
+    }
+
+    output = {
+        "result": int
+    }
+
+    @classmethod
+    def validate_input(cls, a, b):
+        return {
+            "a": (a >= 0, "Value 'a' must be positive"),
+            "b": (b < 0,  "Value 'b' must be negative")
+        }
+
+    @classmethod
+    def run(cls, a, b):
+        return a + b
