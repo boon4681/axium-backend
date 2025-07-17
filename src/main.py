456915e86b7af1c1d9f9ad3ba38216423873004f
applentk <tanoobguy12@gmail.com>
2025-07-17 20:58:40 +0700

    update: CORS middleware

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from axium.core import Axium
 from api.routes import execute, node, project
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 Axium.setup()
 
@@ -10,6 +11,14 @@ api = FastAPI(root_path="/api/v1")
 api.include_router(node.router)
 api.include_router(project.router)
 api.include_router(execute.router)
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode='asgi')
 sio_app = socketio.ASGIApp(sio) 
